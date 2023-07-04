a6a0ae05af6e82805c78254cd252b50c2ca4f100
Saoa35 <saoamah@gmail.com>
2023-07-04 17:19:20 +0400

    stylized MessageField component

--- client/src/components/chat/components/messageField/messageField.jsx
+++ client/src/components/chat/components/messageField/messageField.jsx
@@ -1,9 +1,11 @@
+import "./messageField.css";
+
 export const MessageField = () => {
   return (
     <div className="message-field">
-      <form>
+      <form className="message_form">
         <input type="text" className="user-message" />
-        <button>Sent</button>
+        <button className="sent_btn">Sent</button>
       </form>
     </div>
   );
--- client/src/components/chat/index.jsx
+++ client/src/components/chat/index.jsx
@@ -1,5 +1,5 @@
 import { Body } from "./components/body/body";
-import { MessageField } from "./components/messageField";
+import { MessageField } from "./components/messageField/messageField";
 import { Sidebar } from "./components/sidebar/sidebar";
 import "./styles.css";
 

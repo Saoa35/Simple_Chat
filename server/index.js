d868e372a6b52153bb6cc06853883bc3a3c2ced4
Saoa35 <saoamah@gmail.com>
2023-07-04 10:11:43 +0400

    finished server created with socketIO

--- server/index.js
+++ server/index.js
@@ -3,12 +3,28 @@ const express = require("express");
 const app = express();
 const PORT = 5000;
 
+const http = require("http").Server(app);
+const cors = require("cors");
+
+const socketIO = require("socket.io")(http, {
+  cors: {
+    origin: "http://localhost:5173",
+  },
+});
+
 app.get("api", (req, res) => {
   res.json({
     message: "success",
   });
 });
 
-app.listen(PORT, () => {
+socketIO.on("connection", (socket) => {
+  console.log(`${socket.id} user connected`);
+  socket.on("disconnect", () => {
+    console.log(`${socket.id} disconnect`);
+  });
+});
+
+http.listen(PORT, () => {
   console.log(`Server started on ${PORT} port`);
 });

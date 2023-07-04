a292f34fd3e89db2b4983bd43b498dc64d5c0014
Saoa35 <saoamah@gmail.com>
2023-07-04 16:32:08 +0400

    stylized body component

--- client/src/components/chat/components/body/body.jsx
+++ client/src/components/chat/components/body/body.jsx
@@ -1,12 +1,14 @@
+import "./body.css";
+
 export const Body = () => {
   return (
     <>
-      <header className="header">
-        <button className="escape_btn">Leave chat</button>
+      <header className="body_header">
+        <button className="escape_btn">Leave</button>
       </header>
-      <div className="container">
+      <div className="body_container">
         <div className="chats">
-          <p className="sender_name">You</p>
+          <p className="sender-name">You</p>
           <div className="message-sender">
             <p>Hello</p>
           </div>
